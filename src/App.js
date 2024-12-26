4e68784a021964062d40cbead837cd9f5b75365c
wassupdee <daichi.hirohata.japan@gmail.com>
2024-12-26 11:40:21 +0900

    Navbarを表示する

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
   const title = 'Welcome to the new blog';
@@ -8,18 +9,9 @@ function App() {
 
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{ title }</h1>
-        <p>Liked { likes } times</p>
-
-        {/* <p>{ person }</p> */}
-
-        <p>{ 10 }</p>
-        <p>{ 'hello, ninjas' }</p>
-        <p>{ [1,2,3,4,5] }</p>
-        <p>{ Math.random() * 10 }</p>
-
-        <a href={link}>Google Site</a>
+        <h1>App Component</h1>
       </div>
     </div>
   );

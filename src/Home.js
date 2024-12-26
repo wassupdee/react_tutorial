ba852c14cbba65c69aabbe550eaa196b3bc3289b
wassupdee <daichi.hirohata.japan@gmail.com>
2024-12-26 13:18:30 +0900

    クリックで発動する関数を定義

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,13 @@
 const Home = () => {
+
+  const handleClick = (name) => {
+    console.log('hello ' + name);
+  }
+
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={() => handleClick('mario')}>Click me!</button>
     </div>
   );
 }

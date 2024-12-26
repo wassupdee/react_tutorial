28881b24e99a9c3dfab2f161526dc1785869ae3a
wassupdee <daichi.hirohata.japan@gmail.com>
2024-12-26 11:42:47 +0900

    Homeページを表示する

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import './App.css';
 import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-  //const person = { name: 'yoshi', age: 30 };
-  const link = "http://www.google.com";
-
   return (
     <div className="App">
       <Navbar />
       <div className="content">
-        <h1>App Component</h1>
+        <Home />
       </div>
     </div>
   );
